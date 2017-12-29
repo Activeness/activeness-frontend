51a0792accaa4d08b2c1fdff8b6f3148eb4cd976
Mario Linz <gelight@gmx.de>
2017-12-30 00:37:11 +0100

    Updated main navigation. Added new language chooser.

--- app/components/header-nav/component.js
+++ app/components/header-nav/component.js
@@ -1,11 +1,67 @@
 import Component from '@ember/component';
+import { A } from "@ember/array"
+import { computed } from "@ember/object"
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+    
+    /**
+     * Defines the localizations service 'intl'
+     */
+    intl: service(),
 
     /**
-     * @property {array} items
+     * Defines all main navigation items
+     * 
+     * @property {Ember.array} items
      * @default []
      */
-    items: []
+    items: A([
+        {
+            title: "app.navigation.home.title",
+            description: "app.navigation.home.description",
+            link: "index"
+        },
+        {
+            title: "app.navigation.today.title",
+            description: "app.navigation.today.description",
+            link: "today"
+        },
+        {
+            title: "app.navigation.random.title",
+            description: "app.navigation.random.description",
+            link: "random"
+        }
+    ]),
+
+    locales: computed('intl.locales', function() {
+        return this.get('intl').get('locales').map((locale) => {
+            return {
+                locale: locale,
+                lang: locale.split('-')[1]
+            }
+        });
+    }),
+
+    currentLocale: computed('intl.locale', function() {
+        return this.get('intl').get('locale');
+    }),
+
+    currentLang: computed('intl.locale', function() {
+        return this.get('intl').get('locale')[0].split('-')[1];
+    }),
+
+    /**
+     * Defines all component related action methods
+     * 
+     * @property {object} actions
+     */
+    actions: {
+
+        setLocale(locale) {
+            this.get('intl').setLocale(locale.locale);
+        }
+
+    }
 
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,37 +1,12 @@
 import Route from '@ember/routing/route';
-import Ember from 'ember';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
 
-    intl: Ember.inject.service(),
+    intl: service(),
 
     beforeModel() {
-        return this.get('intl').setLocale(['de-de', 'en-us']);
-    },
-    
-    model() {
-        return Ember.RSVP.hash({
-            navigationItems: [
-                {
-                    title: "app.navigation.home.title",
-                    description: "app.navigation.home.description",
-                    link: "index",
-                    active: true
-                },
-                {
-                    title: "app.navigation.today.title",
-                    description: "app.navigation.today.description",
-                    link: "today",
-                    active: false
-                },
-                {
-                    title: "app.navigation.random.title",
-                    description: "app.navigation.random.description",
-                    link: "random",
-                    active: false
-                }
-            ]
-        });
+        return this.get('intl').setLocale('de-de');
     }
 
 });
