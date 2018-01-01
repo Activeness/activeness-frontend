b7c68a05422e8412103fcc85f2a89a3eef89c617
Mario Linz <gelight@gmx.de>
2018-01-01 14:59:06 +0100

    Prepared footer comonent.

---
+++ app/components/footer-section/component.js
@@ -0,0 +1,12 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+  /**
+   * Defines the tag name of the component ember-view.
+   *
+   * @property {string} tagName
+   */
+  tagName: 'footer'
+
+});
