ba10fd330eeadabf78ba879ef02d41e8c532e3be
Mario Linz <mario.linz@rewe-digital.com>
2018-01-03 16:11:27 +0100

    Prepared main-image component.

---
+++ app/components/main-image/component.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    /**
+     * 
+     */
+    classNames: ['main-image'],
+
+    /**
+     * Defines the image source for the component
+     * 
+     * @property {string} imgSrc
+     * @default null
+     */
+    imgSrc: 'https://static.pexels.com/photos/20974/pexels-photo.jpg'
+
+});
