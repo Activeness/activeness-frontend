1ca3295b09d891ea0f342c2c886298cff38fd817
Mario Linz <gelight@gmx.de>
2018-01-05 00:27:06 +0100

    Adapted footer templates etc...

--- app/components/footer-section/component.js
+++ app/components/footer-section/component.js
@@ -1,37 +1,63 @@
 import Component from '@ember/component';
 import { A } from "@ember/array"
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
 
-  /**
-   * Defines the tag name of the component ember-view.
-   *
-   * @property {string} tagName
-   */
-  tagName: 'footer',
+    intl: service(),
 
-  /**
+    /**
+     * Defines the tag name of the component ember-view.
+     *
+     * @property {string} tagName
+     */
+    tagName: 'footer',
+
+    didReceiveAttrs() {
+        this._super(...arguments);
+        this.set('latestActivityViews', A([
+            {
+                title: "Lorem ipsum dolor sit amet",
+                date: this.get('intl').formatDate(new Date()),
+                img: "https://www.nordevent.de/files/content/Eventlocations/Hamburg/Penthouse-Elb-Panorama/Bildergalerie-Elb-Panorama/EP-1.jpg"
+            },
+            {
+                title: "Lorem ipsum",
+                date: this.get('intl').formatDate(new Date()),
+                img: "https://www.nordevent.de/files/content/Eventlocations/Hamburg/Penthouse-Elb-Panorama/Bildergalerie-Elb-Panorama/EP-1.jpg"
+            },
+            {
+                title: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+                date: this.get('intl').formatDate(new Date()),
+                img: "https://www.nordevent.de/files/content/Eventlocations/Hamburg/Penthouse-Elb-Panorama/Bildergalerie-Elb-Panorama/EP-1.jpg"
+            }
+        ]));
+    },
+
+    /**
      * Defines all footer navigation items
      * 
      * @property {Ember.array} footerNavigationItems
      * @default []
      */
     footerNavigationItems: A([
-      {
-          title: "app.navigation.imprint.title",
-          description: "",
-          link: "imprint"
-      },
-      {
-          title: "app.navigation.dataprotection.title",
-          description: "",
-          link: "dataprotection"
-      },
-      {
-          title: "app.navigation.disclaimer.title",
-          description: "",
-          link: "disclaimer"
-      }
-  ])
+        {
+            title: "app.navigation.imprint.title",
+            description: "",
+            link: "imprint"
+        },
+        {
+            title: "app.navigation.dataprotection.title",
+            description: "",
+            link: "dataprotection"
+        },
+        {
+            title: "app.navigation.disclaimer.title",
+            description: "",
+            link: "disclaimer"
+        }
+    ]),
+    
+    latestActivityViews: null
 
 });
