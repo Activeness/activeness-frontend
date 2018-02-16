b76e304867ba6462bbc69bfd9c1b91b263b9a1d7
Mario Linz <mario.linz@rewe-digital.com>
2018-02-16 15:32:40 +0100

    update css

--- app/routes/activities.js
+++ app/routes/activities.js
@@ -12,11 +12,7 @@ export default Route.extend({
     model(transition) {
         return RSVP.hash({
             activities: this.get('store').query('activity', { cid: transition.cid }),
-            categories: this.get('store').findAll('category').then((categories)=>{
-                return categories.filter((category)=>{
-                    return category.get('title').indexOf('*') === -1;
-                });
-            })
+            categories: this.get('store').findAll('category')
         });
     }
 
